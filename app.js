eb3826c651a9c15292f223c2a5937d7157b70144
salespaulo <salespaulo08@gmail.com>
2016-04-15 04:47:34 -0300

    Fix bootstrap and jquery config

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ var app     = express();
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use('/', express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
-app.post('/login', api.login);
+app.post('/api/login', api.login);
 
 app.listen((process.env.PORT || 3000), function () {
   console.log('Example app listening on port 3000!');
